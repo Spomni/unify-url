0f667f8f7a0f2feabbc96c830f16090a521d404f
spomni <mikhail.snitko@gmail.com>
2021-08-17 09:41:46 +0300

    test: Describe unifyUrl() test cases.

---
+++ test/unifyUrl.spec.js
@@ -0,0 +1,13 @@
+describe('unifyUrl(url)', () => {
+
+  it('Should throw an error if the url is not string')
+  it('Should throw an error if the url contains more than one characters "?".')
+  it('Should throw an error if the url contains more than one characters "#" in the search part of them.')
+  it('Should throw an error if any query param is not uri encoded.')
+
+  it('Should not change the url part before query.')
+  it('Should not change an url hash.')
+ 
+  it('Should remove empty query params.')
+  it('Should sort query params.')
+})
